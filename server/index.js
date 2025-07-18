e266fa79fc041e6b4ed5b6cc711a6e4c40b2efdb
mxrxxsv <130885953+mxrxxsv@users.noreply.github.com>
2025-07-18 22:16:33 +0800

    Updating Last

--- server/index.js
+++ server/index.js
@@ -116,8 +116,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "dist")));
 
   app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "dist", "index.html"));
-  });
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 }
 
 
