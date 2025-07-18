d093b839702826e9710fc2b69c1101de353c263e
mxrxxsv <130885953+mxrxxsv@users.noreply.github.com>
2025-07-18 23:13:48 +0800

    lasthope

--- server/index.js
+++ server/index.js
@@ -115,7 +115,7 @@ io.on("connection", (socket) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "dist")));
 
-  app.get("/*", (req, res) => {
+  app.get('/{*splat}', (req, res) => {
     res.sendFile(path.join(__dirname, "dist", "index.html"));
   });
 }
